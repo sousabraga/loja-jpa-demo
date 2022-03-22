f7a2227241d93db512bac44d4174a3361e4cf260
Matheus Braga <matheusssbraga@gmail.com>
2022-03-21 21:40:14 -0300

    Configurando arquivos de persistência e iniciando o CRUD

---
+++ src/main/java/br/com/alura/loja/crud/CadastroProduto.java
@@ -0,0 +1,29 @@
+package br.com.alura.loja.crud;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.alura.loja.modelo.Produto;
+
+public class CadastroProduto {
+	
+	private static final String PERSISTENCE_UNIT_NAME = "loja";
+
+	public static void main(String[] args) {
+		Produto celular = new Produto("Xiaomi", "Muito legal", BigDecimal.valueOf(800));
+		EntityManagerFactory EntityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		EntityManager entityManager = EntityManagerFactory.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		
+		entityManager.persist(celular);
+		
+		entityManager.getTransaction().commit();
+		
+		entityManager.close();
+	}
+
+}
