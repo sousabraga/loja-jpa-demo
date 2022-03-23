d24fa2c829a4d47ed23ae397ad0e7d195dd93439
Matheus Braga <matheusssbraga@gmail.com>
2022-03-23 17:11:01 -0300

    Adding enumerated example

--- src/main/java/br/com/alura/loja/crud/CadastroProduto.java
+++ src/main/java/br/com/alura/loja/crud/CadastroProduto.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 
 public class CadastroProduto {
@@ -13,7 +14,7 @@ public class CadastroProduto {
 	private static final String PERSISTENCE_UNIT_NAME = "loja";
 
 	public static void main(String[] args) {
-		Produto celular = new Produto("Xiaomi", "Muito legal", BigDecimal.valueOf(800));
+		Produto celular = new Produto("Xiaomi", "Muito legal", BigDecimal.valueOf(800), Categoria.CELULARES);
 		EntityManagerFactory EntityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
 		EntityManager entityManager = EntityManagerFactory.createEntityManager();
 		
--- src/main/java/br/com/alura/loja/modelo/Produto.java
+++ src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -1,8 +1,11 @@
 package br.com.alura.loja.modelo;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -22,10 +25,17 @@ public class Produto {
 	
 	private BigDecimal preco;
 	
-	public Produto(String nome, String descricao, BigDecimal preco) {
+	private LocalDate dataCadastro = LocalDate.now();
+	
+	@Enumerated(EnumType.STRING)
+	private Categoria categoria;
+	
+	
+	public Produto(String nome, String descricao, BigDecimal preco, Categoria categoria) {
 		this.nome = nome;
 		this.descricao = descricao;
 		this.preco = preco;
+		this.categoria = categoria;
 	}
 
 	public Long getId() {
